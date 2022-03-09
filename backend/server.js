c34257b4128e82a2ad2d677aa09a04bd35cdf278
hlcostard <hlcostard@gmail.com>
2022-03-09 10:29:16 -0300

    formatted backend JS files, added frontend UI

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
-const {errorHandler} = require('./middleware/errorMiddleware');
-const connectDB = require('./config/db');
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -10,13 +10,13 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals', require('./routes/goalsRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
+app.use("/api/goals", require("./routes/goalsRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server started on ${port}`)
+  console.log(`Server started on ${port}`);
 });
